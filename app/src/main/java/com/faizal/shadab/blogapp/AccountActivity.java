25e2f02acd7a1b455df597212f1d17139f80b0bb
shada <shadabfaizal7@gmail.com>
2019-01-27 13:02:28 +0530

    crop image and setting to imageView is implemented (bugs may be there)

--- app/src/main/java/com/faizal/shadab/blogapp/AccountActivity.java
+++ app/src/main/java/com/faizal/shadab/blogapp/AccountActivity.java
@@ -1,10 +1,14 @@
 package com.faizal.shadab.blogapp;
 
 import android.Manifest;
+import android.content.ActivityNotFoundException;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Build;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
@@ -20,6 +24,7 @@ import android.widget.Toast;
 
 import com.edmodo.cropper.CropImageView;
 import com.google.firebase.auth.FirebaseAuth;
+import com.theartofdev.edmodo.cropper.CropImage;
 
 
 public class AccountActivity extends AppCompatActivity {
@@ -28,6 +33,8 @@ public class AccountActivity extends AppCompatActivity {
     EditText edtUpdateName;
     Button btnSaveChange;
 
+    Uri imageUri = null;
+    Uri resultUri = null;
 
     FirebaseAuth firebaseAuth;
     Toolbar toolbar;
@@ -59,7 +66,9 @@ public class AccountActivity extends AppCompatActivity {
                     ActivityCompat.requestPermissions(AccountActivity.this, PERMISSION_READ_WRITE, REQUEST_PERMISSION_CODE);
                 }else {
                     //Permissions Granted
-
+                    Intent photoPickerIntent = new Intent(Intent.ACTION_GET_CONTENT);
+                    photoPickerIntent.setType("image/*");
+                    startActivityForResult(photoPickerIntent, 1);
 
                 }
 
@@ -100,6 +109,25 @@ public class AccountActivity extends AppCompatActivity {
             Toast.makeText(AccountActivity.this, "Permission Granted", Toast.LENGTH_SHORT).show();
         } else {
             Toast.makeText(AccountActivity.this, "Permission Denied", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 1 && resultCode == RESULT_OK){
+            imageUri = data.getData();
+            CropImage.activity(imageUri).setAspectRatio(1,1)
+                    .start(AccountActivity.this);
+        }
+        if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE) {
+            CropImage.ActivityResult result = CropImage.getActivityResult(data);
+            if (resultCode == RESULT_OK) {
+                resultUri = result.getUri();
+                circleImageView.setImageURI(resultUri);
+            } else if (resultCode == CropImage.CROP_IMAGE_ACTIVITY_RESULT_ERROR_CODE) {
+                Exception error = result.getError();
+            }
         }
     }
 }
